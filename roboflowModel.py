7bf3cd64a79b25e1fa0c7fc73c518b235533694f
mehtadak@msu.edu <110349964+mehtadak@users.noreply.github.com>
2025-02-01 15:36:31 -0500

    Rock, Paper and Scissors
    roboflow model which detects rock, paper and scissors

---
+++ roboflowModel.py
@@ -0,0 +1,14 @@
+from inference import InferencePipeline
+from inference.core.interfaces.stream.sinks import render_boxes
+
+api_key = "T9dyaU5pnxSWychmChZi"
+
+pipeline = InferencePipeline.init(
+    model_id="rock-paper-scissors-sxsw/11",
+    video_reference=0,
+    on_prediction = render_boxes,
+    api_key=api_key,
+)
+
+pipeline.start()
+pipeline.join()
