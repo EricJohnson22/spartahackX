564c69a8655441c28f0ac88586b789c45d8f9ca0
Aneesh Kapole <aneeshkapole.9@gmail.com>
2025-02-01 12:53:12 -0500

    Initial Commit for GUI
    draw basic window frame. Set default size

---
+++ window.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+# create the main window frame
+root = tk.Tk()
+root.title("SpartaHackX")
+
+# set default window size (width*height)
+root.geometry(f"{800}x{600}")
+
+# start window
+root.mainloop()
